48439f693d9cc2473423137030d8d0890ee06d75
julian-perge <julianperge@gmail.com>
2021-12-04 05:03:57 -0500

    Trying async checking for custom API cards. Moved config to separate class.

---
+++ Configs/EvolveRandomlyConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace SigilADay_julianperge
+{
+	public partial class Plugin
+	{
+		public int GetNumCardsForRandomEvolve()
+			=> Config.Bind("EvolveRandomly", "Sets of cards to modify", 1).Value;
+
+		public string GetCardForRandomEvolve(int index)
+			=> Config.Bind("EvolveRandomly", "Card" + (index + 1),
+				"Geck-Skunk,RavenEgg",
+				"Set list of possible cards to evolve into.\n" +
+				"Card to set specified evolutions to, has a hyphen before the other cards.\n"
+				+ "e.g. Geck-Skink,Raven will set the Geck card with 2 possible evolutions.").Value;
+		
+		
+	}
+}
--- sigils/EvolveRandomly.cs
+++ sigils/EvolveRandomly.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using APIPlugin;
 using BepInEx.Bootstrap;
-using BepInEx.Configuration;
 using DiskCardGame;
 using UnityEngine;
 using Resources = SigilADay_julianperge.Properties.Resources;
@@ -12,20 +12,8 @@ namespace SigilADay_julianperge
 {
 	public partial class Plugin
 	{
-		public int GetNumCards()
-			=> Config.Bind(PluginName, "Sets of cards to modify", 1).Value;
-
-		public string GetCard(int index)
-			=> Config.Bind(PluginName, "Card" + (index + 1),
-				"Geck-Skunk,RavenEgg",
-				"Set list of possible cards to evolve into.\n" +
-				"Card to set specified evolutions to, has a hyphen before the other cards.\n"
-				+ "e.g. Geck-Skink,Raven will set the Geck card with 2 possible evolutions.").Value;
-
-		private NewAbility AddRandomEvolve()
+		private NewAbility AddEvolveRandomly()
 		{
-			InitializeRandomEvolveCardListConfig();
-
 			// setup ability
 			const string rulebookName = "Evolve Randomly";
 			const string rulebookDescription =
@@ -42,14 +30,14 @@ namespace SigilADay_julianperge
 			return newAbility;
 		}
 
-		private void InitializeRandomEvolveCardListConfig()
+		private void InitializeEvolveRandomlyCardListConfig()
 		{
-			int numOfCards = GetNumCards();
+			int numOfCards = GetNumCardsForRandomEvolve();
 
 			for (var i = 0; i < numOfCards; i++)
 			{
-				string cardWithHyphenAndCommas = GetCard(i);
-				Plugin.Log.LogDebug($"Card #[{i + 1}] has [{cardWithHyphenAndCommas}]");
+				string cardWithHyphenAndCommas = GetCardForRandomEvolve(i);
+				Log.LogDebug($"Card #[{i + 1}] has [{cardWithHyphenAndCommas}]");
 
 				string[] split = cardWithHyphenAndCommas.Split('-');
 
@@ -82,18 +70,44 @@ namespace SigilADay_julianperge
 
 		private bool TryCheckAPICards(string _name)
 		{
-			return Chainloader.PluginInfos.ContainsKey(APIGUID) && CheckForAPICard(_name);
+			return Chainloader.PluginInfos.ContainsKey(APIGUID)
+			       && Chainloader.PluginInfos.ContainsKey(JSONGUID)
+			       && CheckForAPICard(_name).Result;
 		}
 
+		async Task<bool> WaitForItToWork()
+		{
+			bool succeeded = ScriptableObjectLoader<CardInfo>.allData is not null;
+			while (!succeeded)
+			{
+				// do work
+				succeeded = ScriptableObjectLoader<CardInfo>.allData is not null; // if it worked, make as succeeded, else retry
+				await Task.Delay(1000); // arbitrary delay
+			}
+			return succeeded;
+		}
+		
 		[MethodImpl(MethodImplOptions.NoInlining)]
-		private bool CheckForAPICard(string nameParam)
-			=> CustomCard.cards.Exists(elem => elem.name == nameParam) ||
-			   NewCard.cards.Exists(elem => elem.name == nameParam);
+		private async Task<bool> CheckForAPICard(string nameParam)
+		{
+			bool result = await WaitForItToWork();
+			Plugin.Log.LogDebug("WaitForItToWork done:" + result); // true or false
+			return CustomCard.cards.Exists(elem => elem.name == nameParam) ||
+			       NewCard.cards.Exists(elem => elem.name == nameParam);
+		}
 	}
 
 
 	public class EvolveRandomly : Evolve
 	{
+		public static void AddCardToDictionary(string name, List<string> evolutionCards)
+		{
+			CardsWithSpecifiedEvolutions.Add(name,
+				evolutionCards
+					.Select(CardLoader.GetCardByName).ToList()
+			);
+		}
+
 		public static readonly Dictionary<string, List<CardInfo>> CardsWithSpecifiedEvolutions = new();
 		public static Ability ability;
 		public override Ability Ability => ability;
