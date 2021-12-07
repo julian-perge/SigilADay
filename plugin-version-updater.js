c9cc9495924d3d54ff4461aa47853a03270498fd
julian-perge <julianperge@gmail.com>
2021-12-07 01:19:54 -0500

    fix: fixed finding verison number for Plugin.cs

--- plugin-version-updater.js
+++ plugin-version-updater.js
@@ -1,8 +1,3 @@
-// standard-version-updater.js
-const stringifyPackage = require('stringify-package')
-const detectIndent = require('detect-indent')
-const detectNewline = require('detect-newline')
-
 module.exports.readVersion = function (contents) {
   return GetNumbersFromContents(contents);
 }
@@ -13,8 +8,7 @@ module.exports.writeVersion = function (contents, version) {
 }
 
 function GetNumbersFromContents(contents) {
-  const versionNumberMatch = "\d.\d.\d";
-  const number = contents.toString().match(new RegExp(versionNumberMatch));
-  console.log("Found number! =" + number);
+  const number = contents.match(new RegExp(/\d.\d.\d/));
+  console.log("Found number! = " + number);
   return number;
 }
