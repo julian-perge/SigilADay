bd02c3f5a70cd83045ce877f5564c6d6b5fb798f
julian-perge <julianperge@gmail.com>
2021-12-07 02:54:19 -0500

    Corrected version number

--- Plugin.cs
+++ Plugin.cs
@@ -15,7 +15,7 @@ namespace SigilADay_julianperge
 
 		public const string PluginGuid = "julianperge.inscryption.sigiladay";
 		public const string PluginName = "SigilADay_julianperge";
-		private const string PluginVersion = "1.9.6";
+		private const string PluginVersion = "1.9.4";
 
 		internal static ManualLogSource Log;
 
