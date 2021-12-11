c5f8ecd3fe39ae78c5e5cd1b5cce034e9a264522
julian-perge <julianperge@gmail.com>
2021-12-11 04:25:55 -0500

    fix: Corrected dir to check for CHANGELOG

--- tools/fix-header-levels-in-changelog.sh
+++ tools/fix-header-levels-in-changelog.sh
@@ -2,6 +2,6 @@
 # Workaround for buggy behavior
 # Change '### [' to '## ['
 echo "Running level fixer"
-DIR="./"
-sed -r -e 's/^#{1,3} \[/## [/' -i $DIR/CHANGELOG.md
-sed -r -e 's/^\- /\* /' -i $DIR/CHANGELOG.md
+#DIR="./"
+sed -r -e 's/^#{1,3} \[/## [/' -i ../CHANGELOG.md
+sed -r -e 's/^\- /\* /' -i ../CHANGELOG.md
