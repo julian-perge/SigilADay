45608190ee5c36b2c4f44b982bab916432858f5c
julian-perge <julianperge@gmail.com>
2021-12-11 04:14:01 -0500

    rfc: Changed regex to now look for multiple numbers for either major.minor.patch

--- tools/plugin-version-updater.js
+++ tools/plugin-version-updater.js
@@ -2,13 +2,15 @@ module.exports.readVersion = function (contents) {
   return GetNumbersFromContents(contents);
 }
 
+const regex = /\d{1,3}.\d{1,3}.\d{1,3}/;
+
 module.exports.writeVersion = function (contents, version) {
-  contents = contents.replace(/\d.\d.\d/, version);
+  contents = contents.replace(regex, version);
   return contents;
 }
 
 function GetNumbersFromContents(contents) {
-  const number = contents.match(new RegExp(/\d.\d.\d/));
+  const number = contents.match(new RegExp(regex));
   console.log("Found number! = " + number);
   return number;
 }
